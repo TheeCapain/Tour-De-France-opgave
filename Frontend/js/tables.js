8abf2f2c7325ce8b14ab49226d90f8cb4031a949
August <Augusthauerslev@outlook.com>
2022-05-30 11:38:42 +0200

    Tifløjet bootstrap og kan nu lave dynamisk tabel over ryttere

--- Frontend/js/tables.js
+++ Frontend/js/tables.js
@@ -1,7 +1,37 @@
-const bikerTable ="http://localhost:8080/bikers/all"
+const bikerTable = "http://localhost:8080/biker/all"
 
-async function fetchAllBikerS(){
+async function fetchAllBikerS() {
   return fetch(bikerTable).then(response => response.json())
 }
 
-console.log(fetchAllBikerS())
+async function printBikerTable() {
+  const bikerList = await fetchAllBikerS();
+
+  for (let biker of bikerList) {
+    let parent = document.getElementById("parent")
+
+
+    console.log(biker)
+    const tableRow = document.createElement("tr")
+
+    let td1 = document.createElement("td")
+    td1.textContent = biker.bikerId;
+    let td2 = document.createElement("td")
+    td2.textContent = biker.name;
+    let td3 = document.createElement("td")
+    td3.textContent = biker.teamName;
+    let td4 = document.createElement("td")
+    td4.textContent = biker.mountainPoints
+
+      parent.appendChild(tableRow)
+    tableRow.appendChild(td1);
+    tableRow.appendChild(td2)
+    tableRow.appendChild(td3)
+    tableRow.appendChild(td4)
+  }
+}
+
+
+printBikerTable()
+
+
